b41df99cb308348cf4cb3bdef99e579111d118b2
Kouta-Enter <kouta2006n@gmail.com>
2025-08-21 01:29:27 +0000

    step1-課題2を追加しました。

---
+++ todo.py
@@ -0,0 +1,37 @@
+"""todoリスト管理スクリプト step1 課題2"""
+import json
+import os
+if not os.path.exists('./CAWAL/basicassignment01/todo_save.json'):
+    with open('./CAWAL/basicassignment01/todo_save.json', 'w', encoding='utf-8') as f:
+        json.dump({}, f)
+
+while 1:
+    user_input=input(">").split()
+    with open('./CAWAL/basicassignment01/todo_save.json', 'r', encoding="utf-8") as f:
+        s=f.read()
+        todo_dict = json.loads(s)
+    if user_input[0]=='list':
+        if len(todo_dict)==0:
+            print("(タスクなし)")
+        for v in todo_dict.keys():
+            print("[",v,"]",todo_dict[v])
+    elif user_input[0]=='done':
+        try:
+            if int(user_input[1])<=len(todo_dict)\
+                and int(user_input[1])>0:
+                if len(todo_dict)>1:
+                    for i in range(int(user_input[1]),len(todo_dict)):
+                        todo_dict[str(i)]=todo_dict[str(i+1)]
+                del todo_dict[str(len(todo_dict))]
+                with open('./CAWAL/basicassignment01/todo_save.json', 'w', encoding="utf-8") as f:
+                    json.dump(todo_dict, f)
+        except ValueError:
+            print("error:",user_input[0],">>",user_input[1],"<<\nexpected an index number")
+    elif user_input[0]=='add':
+        todo_dict[str(len(todo_dict)+1)]=user_input[1]
+        with open('./CAWAL/basicassignment01/todo_save.json', 'w', encoding="utf-8") as f:
+            json.dump(todo_dict, f)
+    elif user_input[0]=='q':
+        break
+    else:
+        print("error!")
