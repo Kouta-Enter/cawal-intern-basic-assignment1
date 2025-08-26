93b5e7dabdf9fd86f6aafa38420fb949cf700c9b
Kouta-Enter <kouta2006n@gmail.com>
2025-08-26 05:19:46 +0000

    引数が無い時の処理を追加

--- word_count.py
+++ word_count.py
@@ -2,13 +2,16 @@
 import sys
 import os
 args = sys.argv
-if not os.path.exists(args[1]):
-    print("ファイルがありません")
+if len(args)<2:
+    print('error:読み込むファイルを指定してください。\n$python3 word_count.py >> <<')
 else:
-    with open(args[1], 'r', encoding='utf-8') as f:
-        s = f.read()
-        lines=len(s.splitlines())
-        words=len(s.split())
-        length=len(s)
-    with open('word_count_result.txt', 'w', encoding='utf-8') as f:
-        f.write('行数：'+str(lines) +'\n単語数:'+ str(words)+'\n文字数:'+str(length))
+    if not os.path.exists(args[1]):
+        print("ファイルがありません")
+    else:
+        with open(args[1], 'r', encoding='utf-8') as f:
+            s = f.read()
+            lines=len(s.splitlines())
+            words=len(s.split())
+            length=len(s)
+        with open('word_count_result.txt', 'w', encoding='utf-8') as f:
+            f.write('行数：'+str(lines) +'\n単語数:'+ str(words)+'\n文字数:'+str(length))
